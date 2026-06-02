09839ddf0a294e1734d119b8aa32208e22844e96
Tobias Bollinger <tobias.bollinger@bs.ch>
2026-06-02 15:26:35 +0200

    fixed the pydantic decorator for app config

--- src/bs_translator_backend/utils/app_config.py
+++ src/bs_translator_backend/utils/app_config.py
@@ -27,7 +27,7 @@ class AppConfig(AbstractAppConfig):
     whisper_url: str = Field(description="The URL for the Whisper API")
 
     @field_validator(
-        "openai_api_base_url", "client_url", "docling_url", "whisper_url", mode="after"
+        "llm_api_key", "client_url", "docling_url", "whisper_url", mode="after"
     )
     @classmethod
     def strip_trailing_slash(cls, v: str) -> str:
