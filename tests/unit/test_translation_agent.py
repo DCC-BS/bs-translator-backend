aaf13928561c7f6856ca1feabbd3b70219458935
Tobias Bollinger <tobias.bollinger@gmail.com>
2026-09-15 10:30:38 +0200

    updated docling options (#74)
    * updated docling options
    
    * Bump version to 1.5.3
    
    * - Remove hardcoded OCR language default
    - Make docling_options a required argument
    - Remove unused S106 noqa comments in tests
    
    ---------
    
    Co-authored-by: Tobias Bollinger <tobias.bollinger@bs.ch>

--- tests/unit/test_translation_agent.py
+++ tests/unit/test_translation_agent.py
@@ -20,7 +20,7 @@ def app_config() -> AppConfig:
         client_url="http://localhost:3000",
         docling_url="http://localhost:8004/v1",
         docling_api_key="none",
-        hmac_secret="test-secret",  # noqa: S106
+        hmac_secret="test-secret",
         whisper_url="http://localhost:50001/v1",
     )
 
